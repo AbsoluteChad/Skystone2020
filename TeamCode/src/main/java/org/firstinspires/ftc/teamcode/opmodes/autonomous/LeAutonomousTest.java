93a55ded734a246b1affe1fcb6ede5d4abfe3643
Veena <vkodakirthi@gmail.com>
2020-01-01 14:59:49 -0500

    changed drive mecanum values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/LeAutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/LeAutonomousTest.java
@@ -55,7 +55,7 @@ public class LeAutonomousTest extends LinearOpMode {
 
         if (opModeIsActive()) {
             //driveTrain.driveDistance(0.4, -36, false);
-            driveTrain.driveMecanum(0.4, 180, 12, false, 1);
+            driveTrain.driveMecanum(0.4, 0, 36, false, 1);
         }
     }
 }
