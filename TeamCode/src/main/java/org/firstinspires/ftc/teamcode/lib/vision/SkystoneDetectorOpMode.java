c011e116097864c78cda901c3fe03994e67b8811
Veena <vkodakirthi@gmail.com>
2020-02-10 19:51:40 -0500

    Merge branch 'master' of C:\Users\Veena\Downloads\SkyStone-5.3\Skystone2020 with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/vision/SkystoneDetectorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/vision/SkystoneDetectorOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.lib.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.opencv.core.Core;
@@ -19,7 +20,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import java.util.ArrayList;
 import java.util.List;
 
-@Autonomous(name= "EasyOpenCV skystone detector", group="LinearOpMode")
+@TeleOp(name= "EasyOpenCV skystone detector", group="LinearOpMode")
 public class SkystoneDetectorOpMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -30,7 +31,7 @@ public class SkystoneDetectorOpMode extends LinearOpMode {
     private static int valRight = -1;
 
     private static float rectHeight = .6f/8f;
-    private static float rectWidth = 1.5f/8f;
+    private static float rectWidth = 1.2f/8f;
 
     private static float offsetX = 0f/8f;//changing this moves the three rects and the three circles left or right, range : (-2, 2) not inclusive
     private static float offsetY = 0f/8f;//changing this moves the three rects and circles up or down, range: (-4, 4) not inclusive
@@ -59,17 +60,14 @@ public class SkystoneDetectorOpMode extends LinearOpMode {
         waitForStart();
         runtime.reset();
         while (opModeIsActive()) {
-            telemetry.addData("Values", valLeft + "   " + valMid + "   " + valRight);
-            telemetry.addData("Height", rows);
-            telemetry.addData("Width", cols);
-
+            telemetry.addData("Values", valLeft+"   "+valMid+"   "+valRight);
             telemetry.update();
             sleep(100);
         }
     }
 
-     //detection pipeline
-     static class StageSwitchingPipeline extends OpenCvPipeline {
+    //detection pipeline
+    static class StageSwitchingPipeline extends OpenCvPipeline {
         Mat yCbCrChan2Mat = new Mat();
         Mat thresholdMat = new Mat();
         Mat all = new Mat();
